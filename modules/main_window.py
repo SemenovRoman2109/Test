79864fb4fa41beb54813e1239b4727ffcb2c649e
Роман Семёнов <romansemenov2109@gmail.com>
2024-10-25 10:16:12 +0300

    geometry window

--- modules/main_window.py
+++ modules/main_window.py
@@ -2,7 +2,11 @@ import customtkinter
 
 app = customtkinter.CTk()
 
-width = 1000
+width = 1300
 height = 800
-window_width = app.winfo_screenmmwidth()
-window_height = app.winfo_screenmmheight()
+window_width = app.winfo_screenwidth()
+window_height = app.winfo_screenheight()
+x = int(window_width/2 - width/2)
+y = int(window_height/2 - height/2)
+
+app.geometry(f"{width}x{height}+{x}+{y}")
