06b67607eeeb7a1db5875e133e1b5e7e1b12f9a4
Роман Семёнов <romansemenov2109@gmail.com>
2024-10-25 10:11:34 +0300

    Get size

--- modules/main_window.py
+++ modules/main_window.py
@@ -1,4 +1,8 @@
 import customtkinter
 
 app = customtkinter.CTk()
-app.title("Project")
+
+width = 1000
+height = 800
+window_width = app.winfo_screenmmwidth()
+window_height = app.winfo_screenmmheight()
